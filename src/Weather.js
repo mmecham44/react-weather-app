cdf8c1b707eccd9f57f0bb7b9ac592f1d0b42464
mmecham44 <mary.s.mecham@gmail.com>
2022-01-23 14:39:39 -0700

    Added Weather css styling

--- src/Weather.js
+++ src/Weather.js
@@ -11,10 +11,15 @@ export default function Weather() {
               type="search"
               placeholder="Enter a city..."
               className="form-control"
+              autoFocus="on"
             />
           </div>
           <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary" />
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
           </div>
         </div>
       </form>
@@ -23,15 +28,21 @@ export default function Weather() {
         <li>Wednesday 07:00 </li>
         <li>Mostly Cloudy</li>
       </ul>
-      <div className="row">
+      <div className="row mt-3">
         {" "}
         <div className="col-6">
           {" "}
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/partl_cloudy.png"
-            alt="Mostly cloudy"
-          ></img>
-          26°C
+          <div className="clearfix">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/partl_cloudy.png"
+              alt="Mostly cloudy"
+              className="float-left"
+            ></img>
+            <div className="float-left">
+              <span className="temperature">26</span>
+              <span className="unit">°C</span>
+            </div>
+          </div>
         </div>
         <div className="col-6">
           {" "}
