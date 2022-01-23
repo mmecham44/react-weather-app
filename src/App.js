b1f543443e255496d10b6d02ae9b975395421ff1
mmecham44 <mary.s.mecham@gmail.com>
2022-01-23 14:17:33 -0700

    Weather HTML

--- src/App.js
+++ src/App.js
@@ -1,35 +1,40 @@
+import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://www.shecodes.io/workshops/shecodes-responsive-42ca8482-5476-4b51-8a68-fb27a819d71e/projects/620140"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Mary Mecham
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/mmecham44/react-weather-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          open-sourced on GitHub
-        </a>{" "}
-        and{" "}
-        <a
-          href="https://eager-ardinghelli-346998.netlify.app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          hosted on Netlify
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <Weather />
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.shecodes.io/workshops/shecodes-responsive-42ca8482-5476-4b51-8a68-fb27a819d71e/projects/620140"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Mary Mecham
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/mmecham44/react-weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://eager-ardinghelli-346998.netlify.app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
