f3afcabfa00f3172e3a032b21cec7f18be3259ae
mmecham44 <mary.s.mecham@gmail.com>
2022-01-22 21:44:12 -0700

    Added GitHub and Netlify links

--- src/App.js
+++ src/App.js
@@ -1,5 +1,35 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://www.shecodes.io/workshops/shecodes-responsive-42ca8482-5476-4b51-8a68-fb27a819d71e/projects/620140"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Mary Mecham
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/mmecham44/react-weather-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          open-sourced on GitHub
+        </a>{" "}
+        and{" "}
+        <a
+          href="https://eager-ardinghelli-346998.netlify.app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          hosted on Netlify
+        </a>
+      </footer>
+    </div>
+  );
 }
